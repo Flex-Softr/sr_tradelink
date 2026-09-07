c01c428cf723281aec3f409159b36ef2b40a86ce
MD ABIR MAHMUD <abirmahmud5665@gmail.com>
2026-09-07 11:36:25 +0600

    chore(tooling): setup eslint, prettier, husky, and lint-staged

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,10 +2,6 @@ import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 import { defineConfig, globalIgnores } from "eslint/config";
 
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
-import { defineConfig, globalIgnores } from "eslint/config";
-
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
